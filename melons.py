95860383a77d42c7c7debe7b8592298513ce6be2
Rosie <rosie@riveter18a-md.swna.wdpr.disney.com>
2016-04-15 15:27:21 -0700

    Update get_total method for Christmas melons and international flat fee

--- melons.py
+++ melons.py
@@ -1,21 +1,33 @@
 """This file should have our order classes in it."""
 
-class AbstractMelonOrder (object):
+class AbstractMelonOrder(object):
     """ Abstract class for shared attributes/methods from domestic and international melon orders """
+
+    shipped = False
 
     def __init__(self, species, qty, order_type, tax):
         """Initialize melon order attributes"""
         self.species = species
         self.qty = qty
         self.order_type = None
-        self.shipped = False
-        tax = None
+        self.tax = float()
 
     def get_total(self):
         """Calculate price."""
 
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        christmas_price = base_price * 1.5
+        flat_fee_under10 = 3
+
+        if self.order_type == "international" and self.qty < 10:
+            if self.species == "christmas":
+                total = ((1 + self.tax) * self.qty * christmas_price) + flat_fee_under10
+            else:
+                total = ((1 + self.tax) * self.qty * base_price) + flat_fee_under10
+        elif self.species == "christmas":
+            total = ((1 + self.tax) * self.qty * christmas_price)
+        else:
+            total = (1 + self.tax) * self.qty * base_price
         return total
 
     def mark_shipped(self):
@@ -25,19 +37,15 @@ class AbstractMelonOrder (object):
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
-    def __init__(self):
-        self.order_type = "domestic" # Only for Domestic, different value
-        self.tax = 0.08 # Only for Domestic, different value
+    def __init__(self, species, qty):
+        super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08)
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
-    #country_code = country_code # Only for International
     def __init__(self, species, qty, country_code):
         super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
         self.country_code = country_code
-        # self.order_type = "international" # Only for International, different value
-        # self.tax = 0.17 # Only for International, different value
 
-    def get_country_code(self): # Only for International
+    def get_country_code(self):
         """Return the country code."""
         return self.country_code
